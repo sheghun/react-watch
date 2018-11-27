b2834d4b92d1eae5e2c4edf6fac67c0c580420e4
Oladiran Segun Solomon <sheghunoladiran9@gmail.com>
2018-11-27 12:56:59 +0100

    Create watch.js

---
+++ watch.js
@@ -0,0 +1,35 @@
+process.env.BABEL_ENV = 'development';
+process.env.NODE_ENV = 'development';
+
+const fs = require('fs-extra');
+const paths = require('../config/paths');
+const webpack = require('webpack');
+const config = require('../config/webpack.config.dev.js');
+
+// removes react-dev-utils/webpackHotDevClient.js at first in the array
+// config.entry.shift();
+config.entry = config.entry.filter(
+    entry => !entry.includes('webpackHotDevClient')
+  );
+  
+  config.output.path = paths.appBuild;
+  paths.publicUrl = paths.appBuild + '/';
+
+webpack(config).watch({}, (err, stats) => {
+    if (err) {
+        console.error(err);
+    } else {
+        copyPublicFolder();
+    }
+    console.error(stats.toString({
+        chunks: false,
+        colors: true
+    }));
+});
+
+function copyPublicFolder() {
+    fs.copySync(paths.appPublic, paths.appBuild, {
+        dereference: true,
+        filter: file => file !== paths.appHtml
+    });
+}
